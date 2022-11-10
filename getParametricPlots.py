c4984282d3f94ab5f452174c41683332f83dc871
shravan <skotha1@jhu.edu>
2022-11-10 18:48:14 -0500

    enhancements to aesthetics of the figure

--- getParametricPlots.py
+++ getParametricPlots.py
@@ -13,6 +13,15 @@ import math
 idColumnX = 1
 idColumnY = 3
 idColumnData = 8        # 6-thermalGradient, 7-growthRate, 8-coolingRate
+multiplierForXaxisPlot = 1E6
+multiplierForYaxisPlot = 1E6
+sizeFont1 = 28
+sizeFont2 = 24
+scatterPlotMarkerSize = 50
+fontTimes = {'fontname':'Times New Roman'}
+sizeFont = sizeFont1
+plt.rcParams.update({'font.size': sizeFont2})
+
 
 if idColumnData == 6:
     labelPlot = 'Thermal Gradient (K/m)'
@@ -28,9 +37,10 @@ coordsGridNormalDirection_All = []
 gridData_All = []
 
 #
-out_paths = ['G:\My Drive\AFOSR project\C103_simulations\Data for C103 G.R plots - old\\125W-250mm-s-Data for G.R\\' + 'coords_depth_Temp_G_R_coolingRate_alongCenterline.out', \
-'G:\My Drive\AFOSR project\C103_simulations\Data for C103 G.R plots - old\\175W-600mm-s-Data for G.R\\' + 'coords_depth_Temp_G_R_coolingRate_alongCenterline.out', \
-'G:\My Drive\AFOSR project\C103_simulations\Data for C103 G.R plots - old\\230W-1200mm-s-Data for G.R\\' + 'coords_depth_Temp_G_R_coolingRate_alongCenterline.out']
+out_paths = ['G:\My Drive\AFOSR project\C103_simulations_presentations\Data for C103 G.R plots - old\\125W-250mm-s-Data for G.R\\' + 'coords_depth_Temp_G_R_coolingRate_alongCenterline.out', \
+'G:\My Drive\AFOSR project\C103_simulations_presentations\Data for C103 G.R plots - old\\175W-600mm-s-Data for G.R\\' + 'coords_depth_Temp_G_R_coolingRate_alongCenterline.out', \
+'G:\My Drive\AFOSR project\C103_simulations_presentations\Data for C103 G.R plots - old\\230W-1200mm-s-Data for G.R\\' + 'coords_depth_Temp_G_R_coolingRate_alongCenterline.out']
+
 
 for iFile in range(0,len(out_paths)):
     out_path = out_paths[iFile]
@@ -43,18 +53,25 @@ for iFile in range(0,len(out_paths)):
         for iString in range(0,len(listStrings)):
             if listStrings[iString] != '':
                 listData.append(float(listStrings[iString]))
-        coordsGridScanningDirection_All.append(listData[idColumnX-1])
-        coordsGridNormalDirection_All.append(listData[idColumnY-1])
-        gridData_All.append(listData[idColumnData-1])        
+        coordsGridScanningDirection_All.append(multiplierForXaxisPlot*float(listData[idColumnX-1]))
+        coordsGridNormalDirection_All.append(multiplierForYaxisPlot*(listData[idColumnY-1]))
+        gridData_All.append(listData[idColumnData-1])
     file.close()    
 
 print(gridData_All)
 
 plt.figure()
-sc = plt.scatter(coordsGridScanningDirection_All, coordsGridNormalDirection_All, c = gridData_All, cmap = "jet")
+
+#plt.rc('xtick', labelsize=25)
+#plt.rc('ytick', labelsize=25)
+sc = plt.scatter(coordsGridScanningDirection_All, coordsGridNormalDirection_All, c = gridData_All, cmap = "jet", s=scatterPlotMarkerSize)
 plt.colorbar()
-plt.title(labelPlot)
-plt.xlabel("Length (m)")
-plt.ylabel("Depth (m)")
+#plt.title(labelPlot,**fontTimes)
+plt.xlabel("Length ($\mu$m)",**fontTimes,fontsize=sizeFont)
+plt.ylabel("Depth ($\mu$m)",**fontTimes,fontsize=sizeFont)
+plt.tight_layout()
+
+#ax.set_aspect('equal', adjustable='box')
+
 #plt.axis([xlim_min, xlim_max, ylim_min, ylim_max])
 plt.show()
